7a18b334ea51ca600698ff637f56f13ac1db05a3
John Sevilla <jsevilla274@gmail.com>
2017-04-03 00:17:15 -0400

    Define SavingsAccount subclass

---
+++ SavingsAccount.java
@@ -0,0 +1,69 @@
+/**
+ * SavingsAccount class
+ * Inherits from BankAccount to simulate a savings account.
+ */
+
+public class SavingsAccount extends BankAccount
+{
+	private boolean accountActive = true;		// Indicator of account status
+
+	/**
+	 * Calls superclass constructor to initialize balance and annualRate
+	 */
+
+	public SavingsAccount(double balance, double annualRate)
+	{
+		super(balance, annualRate);
+	}
+
+	/**
+	 * Calculates and checks account status
+	 */
+
+	public boolean checkStatus()
+	{
+		//Minimum balance before account becomes frozen is $25
+		return accountActive = (getBalance() >= 25);
+	}
+
+	/**
+	 * Deposits and determines status
+	 */
+
+	public void deposit(double amount)
+	{
+		super.deposit(amount);
+		checkStatus();
+	}
+
+	/**
+	 * Withdraws given an active account
+	 */
+
+	public void withdraw(double amount)
+	{
+		if (accountActive)
+		{
+			super.withdraw(amount);
+			checkStatus();
+		}
+	}
+
+	/**
+	 * Calculates service charge and processes monthly fees
+	 */
+
+	public void monthlyProcess()
+	{
+		int totalWithdraw = getWithdrawals();
+
+		//Service charge after 4 withdrawals
+		if (totalWithdraw > 4)
+		{
+			//$1 charge for each withdrawal after 4
+			setCharges(totalWithdraw - 4);
+		}
+		super.monthlyProcess();
+	}
+
+}
