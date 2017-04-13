2eeed54460504dce618e7f7a3d50f5d682c13329
John Sevilla <jsevilla274@gmail.com>
2017-04-12 20:49:37 -0400

    Refactor "withdraw" methods and variables to "transaction" and update logic for listener.

--- src/MainController.java
+++ src/MainController.java
@@ -1,10 +1,16 @@
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.text.DecimalFormat;
 
 public class MainController
 {
@@ -31,21 +37,49 @@ public class MainController
     private ImageView logoImage;
 
     private SavingsAccount account;
+    private DecimalFormat money = new DecimalFormat("#,##0.00");
+    private Stage transactionPrompt;
+    private boolean stageflag = true;
 
     public MainController(SavingsAccount account)
     {
         this.account = account;
     }
 
-    public void initialize()
+    public void initialize() throws IOException
     {
         Image logo = new Image(getClass().getResourceAsStream("/assets/logo.png"));
         logoImage.setImage(logo);
+
+        balanceOutput.setText(money.format(account.getBalance()));
+        statusOutput.setText(account.checkStatus() ? "Active" : "Inactive");
+
+        transactionPrompt = new Stage();
+        transactionPrompt.setScene(new Scene(FXMLLoader.load(getClass().getResource("transactionpage.fxml"))));
+        transactionPrompt.initModality(Modality.APPLICATION_MODAL);
+
     }
 
-    public void withdrawButtonListener() throws IOException
+    public void transactionButtonListener(ActionEvent event)
     {
-        System.out.println("withdraw was pressed");
+        //show and wait
+        if (stageflag)
+        {
+            transactionPrompt.initOwner(balanceOutput.getScene().getWindow());
+            stageflag = false;
+        }
+
+        if (event.getSource() == withdrawButton)
+        {
+            transactionPrompt.setTitle("Withdraw");
+        }
+        else if (event.getSource() == depositButton)
+        {
+            transactionPrompt.setTitle("Deposit");
+        }
+        transactionPrompt.showAndWait();
+        //set label to appropriate statement
+        //set the title
     }
 
 }
