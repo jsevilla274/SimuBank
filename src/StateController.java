de87b3a723331cffde8be7b9ddb77e9e49cd342a
John Sevilla <jsevilla274@gmail.com>
2017-04-13 19:06:37 -0400

    Add file chooser functionality to statement controller and add .fxml for statement page

--- src/StateController.java
+++ src/StateController.java
@@ -1,7 +1,11 @@
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.text.Text;
+import javafx.stage.FileChooser;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.text.DecimalFormat;
 
 public class StateController {
@@ -27,5 +31,23 @@ public class StateController {
         statementOutput.setText(info);
     }
 
+    public void saveButtonListener() throws IOException
+    {
+        FileChooser saveLocation = new FileChooser();
+
+        //filters out extensions to only .txt
+        FileChooser.ExtensionFilter filter = new FileChooser.ExtensionFilter("TXT files (*.txt)", "*.txt");
+        saveLocation.getExtensionFilters().add(filter);
+
+        //show save file dialog
+        File file = saveLocation.showSaveDialog(saveButton.getScene().getWindow());
+
+        if (file != null)
+        {
+            FileWriter writer = new FileWriter(file);
+            writer.write(info);
+            writer.close();
+        }
+    }
 
 }
